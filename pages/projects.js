9eb59cc27d0ea1dea6ee6957bf474a7c76930001
Lucas Ricci <llricci@protonmail.com>
2023-03-28 11:12:53 -0300

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -5,12 +5,12 @@ export default function Projects() {
         <div className={styles.container}>
             <h1>Projects</h1>
             <div className={styles.grid_container}>
-                <a target="_blank" href='https://github.com/ManimCommunity/awesome-manim' rel="noopener noreferrer"><img width={400} height={225} src='/thumbnails/awesome-manim.png'/></a>
-                <a target="_blank" href='#' rel="noopener noreferrer"><img width={400} height={225} src='/thumbnails/tv-static.jpg'/></a>
-                <a target="_blank" href='#' rel="noopener noreferrer"><img width={400} height={225} src='/thumbnails/tv-static.jpg'/></a>
-                <a target="_blank" href='#' rel="noopener noreferrer"><img width={400} height={225} src='/thumbnails/tv-static.jpg'/></a>
-                <a target="_blank" href='#' rel="noopener noreferrer"><img width={400} height={225} src='/thumbnails/tv-static.jpg'/></a>
-                <a target="_blank" href='#' rel="noopener noreferrer"><img width={400} height={225} src='/thumbnails/tv-static.jpg'/></a>
+                <a target="_blank" href='https://github.com/ManimCommunity/awesome-manim' rel="noopener noreferrer"><img width={320} height={200} src='/thumbnails/awesome-manim.png'/></a>
+                <a target="_blank" href='#' rel="noopener noreferrer"><img width={320} height={200} src='/thumbnails/tv-static.jpg'/></a>
+                <a target="_blank" href='#' rel="noopener noreferrer"><img width={320} height={200} src='/thumbnails/tv-static.jpg'/></a>
+                <a target="_blank" href='#' rel="noopener noreferrer"><img width={320} height={200} src='/thumbnails/tv-static.jpg'/></a>
+                <a target="_blank" href='#' rel="noopener noreferrer"><img width={320} height={200} src='/thumbnails/tv-static.jpg'/></a>
+                <a target="_blank" href='#' rel="noopener noreferrer"><img width={320} height={200} src='/thumbnails/tv-static.jpg'/></a>
             </div>
         </div>
     )
