db02adb29c7a16ed147cd5d8367cedf4c3a43305
Lucas Ricci <llricci@protonmail.com>
2023-03-28 20:27:17 -0300

    beta

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import App from 'next/app'
 import Head from 'next/head'
-import Layout from '/components/Layout';
+import Layout from '/components/Layout.js';
 
 export default class MyApp extends App {
   render() {
