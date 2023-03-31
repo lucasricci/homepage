1c5cf387756e7751bd89734348a941204a440e87
Lucas Ricci <llricci@protonmail.com>
2023-03-31 16:20:27 -0300

    Adding article page

--- pages/about.js
+++ pages/about.js
@@ -18,43 +18,45 @@ export default function About() {
                 <p className={styles.console}>lucas in ~ λ uname -a</p>
                 <p>I drive Windows on a daily base, but I also have experience around Linux</p>
                 <br></br>
-                <p className={styles.console}>lucas in ~ λ python</p>
-                <p>Python 3.11.2 on dutch-beer</p>
-                <p>Type "help", "copyright", "credits" or "license" for more information.</p>
-                <p>&gt;&gt;&gt; import this</p>
-                <p>The Zen of Python, by Tim Peters</p><br></br>
-                <p className={styles.zen_of_python}>
-                    Beautiful is better than ugly.
-                    Explicit is better than implicit.
-                    Simple is better than complex.
-                    Complex is better than complicated.
-                    Flat is better than nested.
-                    Sparse is better than dense.
-                    Readability counts.
-                    Special cases aren't special enough to break the rules.
-                    Although practicality beats purity.
-                    Errors should never pass silently.
-                    Unless explicitly silenced.
-                    In the face of ambiguity, refuse the temptation to guess.
-                    There should be one-- and preferably only one --obvious way to do it.
-                    Although that way may not be obvious at first unless you're Dutch.
-                    Now is better than never.
-                    Although never is often better than *right* now.
-                    If the implementation is hard to explain, it's a bad idea.
-                    If the implementation is easy to explain, it may be a good idea.
-                    Namespaces are one honking great idea -- let's do more of those!
-                </p>
+                <div className={styles.python}>
+                    <p className={styles.console}>lucas in ~ λ python</p>
+                    <p>Python 3.11.2 on dutch-beer</p>
+                    <p>Type "help", "copyright", "credits" or "license" for more information.</p>
+                    <p>&gt;&gt;&gt; import this</p><br></br>
+                    <p>The Zen of Python, by Tim Peters</p><br></br>
+                    <p>
+                        Beautiful is better than ugly.<br></br>
+                        Explicit is better than implicit.<br></br>
+                        Simple is better than complex.<br></br>
+                        Complex is better than complicated.<br></br>
+                        Flat is better than nested.<br></br>
+                        Sparse is better than dense.<br></br>
+                        Readability counts.<br></br>
+                        Special cases aren't special enough to break the rules.<br></br>
+                        Although practicality beats purity.<br></br>
+                        Errors should never pass silently.<br></br>
+                        Unless explicitly silenced.<br></br>
+                        In the face of ambiguity, refuse the temptation to guess.<br></br>
+                        There should be one-- and preferably only one --obvious way to do it.<br></br>
+                        Although that way may not be obvious at first unless you're Dutch.<br></br>
+                        Now is better than never.<br></br>
+                        Although never is often better than *right* now.<br></br>
+                        If the implementation is hard to explain, it's a bad idea.<br></br>
+                        If the implementation is easy to explain, it may be a good idea.<br></br>
+                        Namespaces are one honking great idea -- let's do more of those!<br></br>
+                    </p>
+                </div>
             </div>
 
             <div className={styles.contact}>
                 <a href="https://linkedin.com/in/lucaslricci" target="_blank">
-                    <img src="https://img.shields.io/badge/-lucasricci-203c63?style=for-the-badge&logo=linkedin" alt="linkedin"/>
+                    <img src="https://img.shields.io/badge/-lucasricci-203c63?style=for-the-badge&logo=linkedin" alt="linkedin" />
                 </a>
                 <a href="mailto:llricci@protonmail.com" target="_blank">
-                    <img src="https://img.shields.io/badge/-llricci-203c63?style=for-the-badge&logo=protonmail&logoColor=white" alt="protonmail"/>
+                    <img src="https://img.shields.io/badge/-llricci-203c63?style=for-the-badge&logo=protonmail&logoColor=white" alt="protonmail" />
                 </a>
                 <a href="https://youtube.com/@lucasricci" target="_blank">
-                    <img src="https://img.shields.io/badge/-lucasricci-203c63?style=for-the-badge&logo=youtube" alt="youtube"/>
+                    <img src="https://img.shields.io/badge/-lucasricci-203c63?style=for-the-badge&logo=youtube" alt="youtube" />
                 </a>
             </div>
         </div>
--- pages/articles.js
+++ pages/articles.js
@@ -1,44 +1,34 @@
-import Blogcard from "../components/Blogcard";
-import fs from 'fs'
-import matter from "gray-matter";
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link';
+import Date from '../components/date';
 
-export default function Home(props) {
-    console.log(props.posts.data)
-    const { posts } = props
+export default function Home({ allPostsData }) {
     return (
-        <div>
-
-            {posts.map((post, index) => (
-                <Blogcard key={index} post={post} />
-            ))}
-
-        </div>
-    );
+        <>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/articles/${id}`}>{title}</Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </>
+    )
 }
+
 export async function getStaticProps() {
-    // Getting all our posts at build time
-
-    // Get all the posts from posts folder
-    const files = fs.readdirSync("posts");
-
-    // Loop over each post to extract the frontmatter which we need
-    const posts = files.map((file) => {
-        // getting the slug here which we will need as a URL query parameter
-        const slug = file.replace(".md", "");
-        // Reading the contents of the file
-        const filecontent = fs.readFileSync(`posts/${file}`, "utf-8");
-        const parsedContent = matter(filecontent);
-        //The parsed content contains data and content we only need the data which is the frontmatter
-        const { data } = parsedContent
-        return {
-            slug,
-            data,
-        };
-    });
-
+    const allPostsData = getSortedPostsData()
     return {
         props: {
-            posts
+            allPostsData
         }
     }
 }
